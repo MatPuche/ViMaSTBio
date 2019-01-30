12bd288d945f2e7da8ee632bdb811f26c16b4ee2
MatPuche <mathilde-puche@hotmail.com>
2019-01-30 10:33:40 +0100

    Creation d'une matrice pour stocker les automates

--- lire_ecrire_fichier.js
+++ lire_ecrire_fichier.js
@@ -14,21 +14,26 @@ document.getElementById('file').onchange = function(){
     }
     line+=1;
 
-    var nbre_auto = 0; //On compte le nombre d'automates
+    //On compte le nombre d'automates et on stocke leurs différents états dans une matrice
+    var auto = [] ;
     while (lines[line].length > 1){
       //On lit les différents automates et les différents états qu'il peuvent prendre
+
+      //Pour les afficher sur la page
       var d = document.createElement('div');
       d.appendChild(document.createTextNode(lines[line]));
       document.body.appendChild(d);
+
+      //Pour les stocker dans la variable auto
       line+=1;
-      nbre_auto+=1;
+      auto+=1;
     }
     rien = document.createElement('br');
     document.body.appendChild(rien);
 
 
     //Pour chacun des automates nous allons lire les différentes transition possibles
-    for (var i=0; i<nbre_auto; i++){
+    for (var i=0, c=auto.length; i<c; i++){
       line+=1;
       while (lines[line].length > 1){
         var d = document.createElement('div');
