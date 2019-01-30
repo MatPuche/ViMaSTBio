93f8bc6454ff4e167fc4e7eb7e42a2f4c268ce73
MatPuche <mathilde-puche@hotmail.com>
2019-01-30 11:20:10 +0100

    Un tableau stocke le nombre d'étatspris par chacun des automates

--- lire_ecrire_fichier.js
+++ lire_ecrire_fichier.js
@@ -1,7 +1,6 @@
 document.getElementById('file').onchange = function(){
 
   var file = this.files[0];
-
   var reader = new FileReader();
   reader.onload = function(progressEvent){
     // By lines
@@ -15,7 +14,8 @@ document.getElementById('file').onchange = function(){
     line+=1;
 
     //On compte le nombre d'automates et on stocke leurs différents états dans une matrice
-    var auto = [] ;
+    var auto = [];
+    var nbre_auto = 0;
     while (lines[line].length > 1){
       //On lit les différents automates et les différents états qu'il peuvent prendre
 
@@ -24,16 +24,24 @@ document.getElementById('file').onchange = function(){
       d.appendChild(document.createTextNode(lines[line]));
       document.body.appendChild(d);
 
-      //Pour les stocker dans la variable auto
+      //Pour les stocker dans la variable auto, on récupère les états possibles
+      var sep = lines[line].split("[");
+      var etats = sep[1].split(',');
+      var nbre_etats = 0 ;
+      for (var i=0, c=etats.length; i<c; i++){
+        nbre_etats+=1;
+      }
+      auto.push(nbre_etats);
       line+=1;
-      auto+=1;
+      nbre_auto+=1;
     }
     rien = document.createElement('br');
     document.body.appendChild(rien);
+    alert(auto);
 
 
     //Pour chacun des automates nous allons lire les différentes transition possibles
-    for (var i=0, c=auto.length; i<c; i++){
+    for (var i=0, c=nbre_auto; i<c; i++){
       line+=1;
       while (lines[line].length > 1){
         var d = document.createElement('div');
