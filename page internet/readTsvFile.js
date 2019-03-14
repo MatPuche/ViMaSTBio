0ec960a9e22e4d3f259a4f75f8b28a7d1c71f927
DorianMalguid <45062380+DorianMalguid@users.noreply.github.com>
2019-03-14 12:36:11 +0100

    Légende interactive
    Cliquer sur un nom de gène dans la légende efface la courbe correspondante sur le graphe. Le graphe est redessiné à chaque fois.

--- page internet/readTsvFile.js
+++ page internet/readTsvFile.js
@@ -2,38 +2,49 @@
  * 
  */
 
+var array = [];
+var readingFinished = false;
+var geneList = [];
+
 document.getElementById("tsvFile").onchange = function() {
 	var file = this.files[0];
 	var reader = new FileReader();
-	var array = [];
 	reader.onload = function() {
 		array = parseTsv(reader.result);
-		var defaultPlotSketch = draw(array);
-		var sketch = new p5(defaultPlotSketch, "sketch-canvas");
-		
+		var defaultPlotSketch = paint(array);
+		sketch = new p5(defaultPlotSketch, "sketch-canvas");
+
 		// Empty the legend div
 		var formulaire = document.getElementById("graphLegend");
-        while (formulaire.hasChildNodes()) {
-            formulaire.removeChild(formulaire.lastChild);
-        }
-        
-        // Add new legend elements and add them support for click events
+		while (formulaire.hasChildNodes()) {
+			formulaire.removeChild(formulaire.lastChild);
+		}
+
+		// Add new legend elements and add them support for click events
 		for (var i = 1; i < array[0].length; i++) {
-			var geneLegend = document.createElement("div");
-			geneLegend.textContent = array[0][i];
-			geneLegend.setAttribute("class", "enabledLegend");
-			geneLegend.id = array[0][i];
-			
-			geneLegend.addEventListener("click", function() {
-				if (this.getAttribute("class") === "enabledLegend") {
-					this.setAttribute("class", "disabledLegend");
-				}
-				else {
-					this.setAttribute("class", "enabledLegend");
-				}
-			}, false);
-			
-			formulaire.appendChild(geneLegend);
+			(function(i) {
+				geneList.push(true);
+				var geneLegend = document.createElement("div");
+				geneLegend.textContent = array[0][i];
+				geneLegend.setAttribute("class", "enabledLegend");
+				geneLegend.id = array[0][i];
+				var geneIndex = Number(geneLegend.textContent.substring(1,
+						geneLegend.textContent.length))-1;
+
+				geneLegend.addEventListener("click", function() {
+					if (this.getAttribute("class") === "enabledLegend") {
+						this.setAttribute("class", "disabledLegend");
+						geneList[geneIndex] = false;
+					} else {
+						this.setAttribute("class", "enabledLegend");
+						geneList[geneIndex] = true;
+					}
+					sketch.remove();
+					repaint(array, geneList);
+				}, false);
+
+				formulaire.appendChild(geneLegend);
+			})(i);
 		}
 	};
 	reader.readAsText(file);
