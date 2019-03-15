400ced5f1aaa4eca3af9469c034af2795435c4fa
MatPuche <mathilde-puche@hotmail.com>
2019-03-15 11:08:43 +0100

    Tests sur la box

--- tests.js
+++ tests.js
@@ -1,20 +1,22 @@
 var xx, yy;
 
 var transitions = {
-  a: [[["0","1"],"b=1","c=1"]],
+  a: [[["0","1"],"b=1","c=1"],[["1","0"],"XXXXX"]],
   b: [[["2","1"],"d=1"], [["2","1"],"c=1"], [["2", "1"]],[["3","1"],"d=1"], [["2","1"],"d=1"]],
   c: [[["1","0"],"d=1"]],
   d: [[["1","0"],"b=1"],[["1","0"],"c=1"]],
   initial_context: " \"a\" = 0, \"b\" = 0, \"c\" = 0, \"d\" = 0",
-  nbre_arcs: 9};
+  nbre_arcs: 10};
 
 var auto = [2,4,2,2];
 
-// oscar
-var texte = "click"
-const reducer = (x, y) => x + " and " +  y
-//
 
+// oscar
+
+var overBox = false;
+var locked = false;
+var texte;
+const reducer = (x, y) => x + " and " +  y;
 
 var coordonnees = [];
 for (i=0; i<transitions['nbre_arcs']; i++){
@@ -27,28 +29,29 @@ function setup() {
 
 
 var dragged = false, first = true;
-var pt1 = document.getElementById("1");
-var pt2 = document.getElementById("2");
 
 
 function draw(){
 
 
-  background(200);  // oscar
+  //background(200);  // oscar
+
+  //Fisrt, we draw automatons.
   for (var i=0, c=auto.length ; i<c;i++){
     automate(auto[i], 140+i*230,140,30,30);
   }
 
+  //Then, we draw arrows that represent transitions of each automata
   var k = 1, n=1, num_arc=0;
   for (var id in transitions){
     if (id!= "initial_context"){
-      //la variable e1 permet de savoir si c'est le premier arc sortant de auto ou non;
-      //On initialise à -1 car le premier est forcément la première transition sortante
+      // the variable e1 enables to know if it is the first arrow coming out of auto or not;
+      //It takes the value of the outgoing state of the precedent drawn arrow and we will check each time if
+      //the outgoing state of the next transition is identic or not
+      // We initialize to -1 because the first one is necessarily the first outgoing transition
       var e1 = -1;
 
-
       for (var i=0, c=transitions[id].length; i<c; i++){
-
 
         var curTrans = transitions[id][i];
         if (curTrans[0][0]==e1){
@@ -57,8 +60,6 @@ function draw(){
         else{
           k=1;
         }
-        var overBox = false;
-        var locked = false;
 
         drawArc(parseInt(curTrans[0][0]), parseInt(curTrans[0][1]), n, k, num_arc, coordonnees, first);
         num_arc+=1;
@@ -74,7 +75,7 @@ function draw(){
 
 
         if(!locked) {
-          fill(200);
+          // fill(200);
           afficheTrans(texte, parseInt(curTrans[0][0]), parseInt(curTrans[0][1]),n,k);
 
         }
@@ -97,6 +98,7 @@ function draw(){
 
 
 
+//To enable arrows to be displaced
 var xi, xj, yi, yj;
 
 var divs = document.getElementsByClassName('arc');
@@ -113,7 +115,6 @@ function stopDrag() {
     dragged = false;
 }
 
-
 document.onmousemove = move;
 document.onmouseup = stopDrag;
 
@@ -123,6 +124,7 @@ document.onmouseup = stopDrag;
 //x1,y1,x2 and y2 allows to place correctly the automata
 function automate(n,x1,y1,x2,y2){
     for (var i=0; i<n; i++){
+      fill(255);
       ellipse(x1,y1+(5-i-1)*50,x2,y2);
     }
     noFill();
@@ -152,15 +154,19 @@ function afficheTrans(texte, e1, e2, a, k){
   var y= 140+(4-e1)*50-(e2-e1)*25;
   var h = 2+(e2-e1)*50;
 
-  if(dist(mouseX, mouseY, x+8,y-h/2-8) < 50) {
+  if (dist(mouseX, mouseY, x+8,y-h/2-8) < 50) {
     fill(255, 0, 0); //rouge
     //noStroke();
-     text(texte,x+8,y-h/2-8);
+     text(texte,x+(e2-e1)*50, y);
 }
 
 }
-//
 
+//function drawArc draw arrows with parameters :
+//e1 : the outgoing state ; e2 : the incoming state ; a : the number of the automata
+//k : the number of the arrow of this automata ; num_arc : the number of the arrow in total (to allow the displacement)
+//coordonnees : array containing the coordinates of each arrow ; first : boolean that indicates if it's the first time arrows are
+//drawn (to create divs if it the case)
 function drawArc(e1,e2,a,k,num_arc, coordonnees, first){
 
   var x = 155+(a-1)*230,
