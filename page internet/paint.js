74abeeb11427c9d2d629795d448745b2588a3f2f
DorianMalguid <45062380+DorianMalguid@users.noreply.github.com>
2019-03-17 21:55:45 +0100

    Liste déroulante pour naviguer entre les courbes du fichier tsv
    Le fichier draw.js s'appelle paint.js pour garder de la cohérence, donc draw.js sert plus à rien

---
+++ page internet/paint.js
@@ -0,0 +1,124 @@
+/**
+ * 
+ */
+
+function paintGraph(array, geneList, curveNo) {
+	var plotGraph = function(p) {
+
+		// Creates and adds the canvas element
+		function addCanvas() {
+			var referenceElement, maxCanvasWidth, canvas;
+
+			// Calculate the canvas dimensions
+			referenceElement = document.getElementById("graph");
+			maxCanvasWidth = referenceElement.clientWidth - 1;
+
+			// Create the canvas
+			canvas = p
+					.createCanvas(0.7 * maxCanvasWidth, 0.49 * maxCanvasWidth);
+
+			return canvas;
+		}
+
+		// Initial setup
+		p.setup = function() {
+			var points, plot;
+
+			// Add the canvas element
+			addCanvas();
+
+			// Prepare the points for the plot
+			points = [];
+
+			for (var graph = 1; graph < array.length; graph++) {
+				points.push([]);
+				for (var gene = 1; gene < array[1].length; gene++) {
+					points[graph - 1].push([]);
+					for (var pointNb = 0; pointNb < array[1][1].length; pointNb++) {
+						points[graph - 1][gene - 1].push(new GPoint(
+								Number(array[graph][0][pointNb]),
+								Number(array[graph][gene][pointNb])));
+					}
+				}
+			}
+
+			// Create a new plot and set its position on the screen
+			plot = new GPlot(p);
+			plot.setPos(0, 0);
+			plot.setOuterDim(p.width, p.height);
+
+			// Add the points
+			for (var i = 1; i < array[0].length; i++) {
+				if (geneList[i - 1]) {
+					plot.addLayer(array[0][i], points[curveNo][i - 1]);
+				}
+			}
+			plot.activatePointLabels();
+
+			// Set the plot title and the axis labels
+			plot.setTitleText("Gene expression");
+			plot.getXAxis().setAxisLabelText(array[0][0]);
+
+			// Draw it!
+			plot.defaultDraw();
+
+			p.noLoop();
+		};
+	};
+
+	graphSketch = new p5(plotGraph, "sketch-canvas");
+}
+
+function repaintGraph(array, geneList, curveNo) {
+	graphSketch.remove();
+	paintGraph(array, geneList, curveNo);
+}
+
+function paintLegend(array) {
+	// Empty the legend div
+	var legendDiv = document.getElementById("graphLegend");
+	while (legendDiv.hasChildNodes()) {
+		legendDiv.removeChild(legendDiv.lastChild);
+	}
+
+	// Add new legend elements and add them support for click events
+	for (var i = 1; i < array[0].length; i++) {
+		(function(i) {
+			selectedGenes.push(true);
+			var geneLegend = document.createElement("div");
+			geneLegend.textContent = array[0][i];
+			geneLegend.setAttribute("class", "enabledLegend");
+			geneLegend.id = array[0][i];
+			var geneIndex = Number(geneLegend.textContent.substring(1,
+					geneLegend.textContent.length)) - 1;
+
+			geneLegend.addEventListener("click", function() {
+				if (this.getAttribute("class") === "enabledLegend") {
+					this.setAttribute("class", "disabledLegend");
+					selectedGenes[geneIndex] = false;
+				} else {
+					this.setAttribute("class", "enabledLegend");
+					selectedGenes[geneIndex] = true;
+				}
+				repaintGraph(array, selectedGenes, displayCurveNo);
+			}, false);
+
+			legendDiv.appendChild(geneLegend);
+		})(i);
+	}
+
+	// Add form element to select which curve of the tsv file will be displayed.
+	var curvesListElt = document.createElement("select");
+	curvesListElt.setAttribute("id", "curvesList");
+	for (var i = 0; i < array.length - 1; i++) {
+		var curveOptionElt = document.createElement("option");
+		curveOptionElt.text = "Curve " + (i + 1);
+		curveOptionElt.value = i;
+		curvesListElt.appendChild(curveOptionElt);
+	}
+	curvesListElt.onchange = function() {
+		displayCurveNo = curvesListElt.selectedIndex;
+		repaintGraph(graphArray, selectedGenes, displayCurveNo);
+	};
+	document.getElementById("graphBox").insertBefore(curvesListElt, document.getElementById("graph"));
+}
--- page internet/readTsvFile.js
+++ page internet/readTsvFile.js
@@ -2,50 +2,18 @@
  * 
  */
 
-var array = [];
-var readingFinished = false;
-var geneList = [];
+var graphArray = [];
+var selectedGenes = [];
 
 document.getElementById("tsvFile").onchange = function() {
 	var file = this.files[0];
 	var reader = new FileReader();
 	reader.onload = function() {
-		array = parseTsv(reader.result);
-		var defaultPlotSketch = paint(array);
-		sketch = new p5(defaultPlotSketch, "sketch-canvas");
-
-		// Empty the legend div
-		var formulaire = document.getElementById("graphLegend");
-		while (formulaire.hasChildNodes()) {
-			formulaire.removeChild(formulaire.lastChild);
-		}
-
-		// Add new legend elements and add them support for click events
-		for (var i = 1; i < array[0].length; i++) {
-			(function(i) {
-				geneList.push(true);
-				var geneLegend = document.createElement("div");
-				geneLegend.textContent = array[0][i];
-				geneLegend.setAttribute("class", "enabledLegend");
-				geneLegend.id = array[0][i];
-				var geneIndex = Number(geneLegend.textContent.substring(1,
-						geneLegend.textContent.length))-1;
-
-				geneLegend.addEventListener("click", function() {
-					if (this.getAttribute("class") === "enabledLegend") {
-						this.setAttribute("class", "disabledLegend");
-						geneList[geneIndex] = false;
-					} else {
-						this.setAttribute("class", "enabledLegend");
-						geneList[geneIndex] = true;
-					}
-					sketch.remove();
-					repaint(array, geneList);
-				}, false);
-
-				formulaire.appendChild(geneLegend);
-			})(i);
-		}
+		graphArray = parseTsv(reader.result);
+		
+		displayCurveNo = 0;
+		paintLegend(graphArray);
+		paintGraph(graphArray, selectedGenes, displayCurveNo);
 	};
 	reader.readAsText(file);
 };
