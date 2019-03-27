af122fc9019bb2cf0ff7f7ff8487da781a25fe0d
oscar <oscar.saadate@gmail.com>
2019-03-27 21:44:06 +0100

    les transitions à gauche sont plus claires Please enter the commit message for your changes. Lines starting

--- Rendu final/scripts/drawAutom.js
+++ Rendu final/scripts/drawAutom.js
@@ -193,7 +193,7 @@ function drawArc(texte, e1, e2, a, k, num_arc, coordonnees, first, p) {
         p.bezier(x1, y1, coordonnees[num_arc][0], coordonnees[num_arc][1] + (e1 - e2 - 1) * 25 + 20, coordonnees[num_arc][0], coordonnees[num_arc][1] - (e1 - e2 - 1) * 25 - 20, x4, y4);
         xx = p.bezierPoint(x1, coordonnees[num_arc][0], coordonnees[num_arc][0], x4, 1 / 2);
         yy = p.bezierPoint(y1, coordonnees[num_arc][1] + (e1 - e2 - 1) * 25 + 20, coordonnees[num_arc][1] - (e1 - e2 - 1) * 25 - 20, y4, 1 / 2);
-        p.fill(0, 255, 0);
+        p.fill(0, 0, 0);
         p.textSize(15);
         p.text(texte, xx + 10, yy);
         p.textSize(10);
@@ -207,9 +207,9 @@ function drawArc(texte, e1, e2, a, k, num_arc, coordonnees, first, p) {
         p.bezier(x1, y1, coordonnees[num_arc][0] - 18, coordonnees[num_arc][1] + (e2 - e1 - 1) * 25 + 20, coordonnees[num_arc][0] - 18, coordonnees[num_arc][1] - (e2 - e1 - 1) * 25 - 20, x4, y4);
         xx = p.bezierPoint(x1, coordonnees[num_arc][0] - 18, coordonnees[num_arc][0] - 18, x4, 1 / 2);
         yy = p.bezierPoint(y1, coordonnees[num_arc][1] + (e2 - e1 - 1) * 25 + 20, coordonnees[num_arc][1] - (e2 - e1 - 1) * 25 - 20, y4, 1 / 2);
-        p.fill(0, 255, 0);
+        p.fill(0, 0, 0);
         p.textSize(15);
-        p.text(texte, xx - 10, yy);
+        p.text(texte, xx - 40, yy);
         p.textSize(10);
       }
     } else {
